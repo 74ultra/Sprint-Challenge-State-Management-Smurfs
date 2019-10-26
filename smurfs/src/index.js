f054f34d11623fa342f4fce48a2902c01314a67f
74ultra <r.barnhill@protonmail.com>
2019-10-25 18:04:13 -0700

    MVP reached

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import 'semantic-ui-css/semantic.min.css'
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import reducer from './reducer/index.js';
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById("root"));
